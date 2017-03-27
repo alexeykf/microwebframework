6561bb47d4b634eb82225c0fb830839b2fb6b5a2
Alexey <alexey-kf@yandex.ru>
2017-03-27 23:15:28 +0300

    add init MainServlet

--- src/main/java/alexeykf/microwebframework/servlet/MainServlet.java
+++ src/main/java/alexeykf/microwebframework/servlet/MainServlet.java
@@ -1,6 +1,8 @@
 package alexeykf.microwebframework.servlet;
 
 import alexeykf.microwebframework.MicroWebFramework;
+import alexeykf.microwebframework.Request;
+import alexeykf.microwebframework.Response;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -14,20 +16,26 @@ public class MainServlet extends HttpServlet {
 
     @Override
     public void init() {
-        framework = new MicroWebFramework();
+        String microWebFrameworkClassName = getInitParameter("MicroWebFrameworkClassName");
+        try {
+            framework = (MicroWebFramework) Class.forName(microWebFrameworkClassName).newInstance();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
-    public void service(HttpServletRequest req, HttpServletResponse resp) {
-
-        String url = req.getRequestURI();
-        String method = req.getMethod();
+    public void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        Request request = Request.createRequest(req);
+        Response response = framework.handle(request);
+        resp.setStatus(response.getStatus());
 
         try (PrintWriter writer = resp.getWriter()) {
-            writer.println(String.format("url: %s", url));
-            writer.println(String.format("method: %s", method));
-        } catch (IOException e) {
-            e.printStackTrace();
+            writer.println(response.getBody());
         }
     }
 }
